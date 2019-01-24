10c7641a0718fb9db47c06820fdb01ec8c4f1a00
shayan <shekshaa.75@gmail.com>
2019-01-24 14:14:58 +0330

    complete switch and while part plus adding some semantic analysis

--- code_generator.py
+++ code_generator.py
@@ -1,5 +1,7 @@
 class CodeGenerator(object):
     def __init__(self, symbol_table):
+        self.while_switch_stack = []
+        self.while_stack = []
         self.semantic_stack = []
         # self.temp = []
         self.temp_pointer = 500
@@ -42,7 +44,7 @@ class CodeGenerator(object):
         self.i += 1
 
     def push_num(self, num):
-        self.semantic_stack.append(num)
+        self.semantic_stack.append('#' + str(num))
 
     def push_id_addr(self, id):
         addr = self.get_id_addr(id, self.scope_stack[-1])
@@ -77,10 +79,70 @@ class CodeGenerator(object):
         self.pop(1)
 
     def while_label(self):
-        self.push(self.i)
+        self.pb[self.i] = ('jp', self.i + 2,)
+        self.i += 1
+        self.while_switch_stack.append(('w', self.i))
+        self.while_stack.append(('w', self.i))
+        self.i += 1
+
+    def break_jp(self):
+        top = self.top_while_switch()
+        if top is not None:
+            self.pb[self.i] = ('jp', top, )
+            self.i += 1
+
+    def continue_jp(self):
+        top = self.top_while()
+        if top is not None:
+            self.pb[self.i] = ('jp', top + 1,)
+            self.i += 1
+
+    def top_while(self):
+        top = None
+        try:
+            top = self.while_stack[-1][1]
+        except:
+            print("Semantic error no while")
+        return top
+
+    def top_while_switch(self):
+        top = None
+        try:
+            top = self.while_switch_stack[-1][1]
+        except:
+            print("Semantic error no while or case")
+        return top
 
     def while_end(self):
         self.pb[self.semantic_stack[-1]] = ('jpf', self.semantic_stack[-2], self.i + 1, )
-        self.pb[self.i] = ('jp', self.semantic_stack[-3],)
-        self.pop(3)
+        top = self.top_while()
+        self.pb[self.i] = ('jp', top + 1,)
         self.i += 1
+        self.pb[top] = ('jp', self.i,)
+        self.pop(2)
+        self.while_stack.pop(-1)
+        self.while_switch_stack.pop(-1)
+
+    def switch_label(self):
+        self.pb[self.i] = ('jp', self.i + 2,)
+        self.i += 1
+        self.while_switch_stack.append(('s', self.i))
+        self.i += 1
+
+    def cmp_save(self):
+        t = self.get_temp()
+        self.pb[self.i] = ('EQ', self.semantic_stack[-1], self.semantic_stack[-2], t)
+        self.i += 1
+        self.pop(1)
+        self.push(t)
+        self.save()
+
+    def end_case(self):
+        self.pb[self.semantic_stack[-1]] = ('jpf', self.semantic_stack[-2], self.i, )
+        self.pop(2)
+
+    def end_switch(self):
+        self.pop(1)
+        top = self.top_while_switch()
+        self.pb[top] = ('jp', self.i)
+        self.while_switch_stack.pop(-1)
