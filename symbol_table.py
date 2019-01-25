0bd2f87241d00659b26d3b3dfea3aa657cf3f19d
shayan <shekshaa.75@gmail.com>
2019-01-26 01:06:06 +0330

    remove search from symbol table to code generator

--- symbol_table.py
+++ symbol_table.py
@@ -33,24 +33,10 @@ class FunctionRow(SymbolTableRow):
         self.return_param = return_param
         self.return_place = return_place
         self.is_closed = False
+        self.counter = 0
 
     def add_param_single(self, token, type, var_type, address):
         self.param_list.append(IDRow(token, type, var_type, address))
 
     def add_param_array(self, token, type, var_type, address, size):
         self.param_list.append(ArrayRow(token, type, var_type, address, size))
-
-
-class SymbolTable(object):
-    def __init__(self):
-        self.table = []
-
-    def search(self, id):
-        for i in range(len(self.table), -1, -1):
-            if self.table[i].token == id:
-                return i, self.table[i].address
-            if type(self.table[i]) == FunctionRow and not self.table[i].is_closed:
-                for param in self.table[i].param_list:
-                    if param.token == id:
-                        return i, param.address
-        return -1, -1
